b0b64e89b5077730d2bf26b0ed2e048a76be472e
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-21 21:28:57 +0530

    Update codeDpTabulation.cpp

--- Longest Common Subsequence/codeDpTabulation.cpp
+++ Longest Common Subsequence/codeDpTabulation.cpp
@@ -2,8 +2,8 @@ int lcsDP(string s1, string s2) {
     int m=s1.length(), n=s2.length();
     vector<vector<int>> dp(m+1, vector<int>(n+1));
     
-    for(int i=0;i<n;i++) dp[0][i]=0;
-    for(int i=0;i<m;i++) dp[i][0]=0;
+    for(int i=0;i<=n;i++) dp[0][i]=0;
+    for(int i=0;i<=m;i++) dp[i][0]=0;
     
     for(int i=1;i<=m;i++) {
         for(int j=1;j<=n;j++) {
