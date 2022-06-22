ea9fd04de379fc66787fff55d1695ec75cbb111e
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-22 16:13:41 +0530

    Update codeDpTabulation.cpp

--- Maximum Cuts/codeDpTabulation.cpp
+++ Maximum Cuts/codeDpTabulation.cpp
@@ -1,7 +1,8 @@
 int maxCutsDP(int n, int a, int b, int c) {
     vector<int> dp(n+1, -1);
     
-    dp[0] = 0;
+    dp[0]=0;
+    
     for(int i=1;i<=n;i++) {
         if(i-a>=0) dp[i] = max(dp[i], dp[i-a]);
         if(i-b>=0) dp[i] = max(dp[i], dp[i-b]);
